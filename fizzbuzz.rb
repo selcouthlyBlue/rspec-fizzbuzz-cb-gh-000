9a565e9db901122ce54a1c729b36d6bff5c15a66
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 05:02:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,8 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    return "FizzBuzz"
-  elsif number % 5 == 0
-    return "Buzz"
-  elsif number % 3 == 0
-    return "Fizz"
-  end
+  result = ""
+  if number % 3 == 0
+    result += "Fizz"
+  if number % 5 == 0
+    result += "Buzz"
+  return result
 end
